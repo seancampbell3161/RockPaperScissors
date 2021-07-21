ded73530c6a7ac0d826f5c14fe49c29cd9427a3c
Sean Campbell <sean.campbell3161@gmail.com>
2021-07-21 18:11:23 -0500

    add function to notify who got to 5 wins first then start again

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 
-function computerMove() {
-
-    let moveSet = ["rock", "paper", "scissors"];
-    let randomNum = Math.floor(Math.random() * moveSet.length);
-    return moveSet[randomNum];
-}
-
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const result = document.querySelector('#result');
 const playerScoreHolder = document.querySelector('#playerScore');
 const computerScoreHolder = document.querySelector('#computerScore');
-const pScore = 0;
-const cScore = 0;
+let pScore = 0;
+let cScore = 0;
+printScore();
 
 rock.addEventListener('click', () => {
     let playerSelection = "rock";
@@ -33,23 +27,37 @@ scissors.addEventListener('click', () => {
     checkWhoWon(playerSelection, computerSelection);
 });
 
+function computerMove() {
+
+    let moveSet = ["rock", "paper", "scissors"];
+    let randomNum = Math.floor(Math.random() * moveSet.length);
+    return moveSet[randomNum];
+}
+
 function checkWhoWon(playerSelection, computerSelection) {
     if(playerSelection == computerSelection) {
         printWhoWon("tie");
     } else if(playerSelection === "rock" && computerSelection === "scissors") {
-        pScore++;
-        printWhoWon("player");      
+        printWhoWon("player");
+        updateScore("player");      
     } else if(playerSelection === "rock" && computerSelection === "paper") {       
         printWhoWon("computer");
+        updateScore("computer");
     } else if(playerSelection === "scissors" && computerSelection === "paper") {    
         printWhoWon("player");
+        updateScore("player");
     } else if(playerSelection === "scissors" && computerSelection === "rock") {   
         printWhoWon("computer");
+        updateScore("computer");
     } else if(playerSelection === "paper" && computerSelection === "rock") {    
         printWhoWon("player");
+        updateScore("player");
     } else {    
         printWhoWon("computer");
+        updateScore("computer");
     }
+    printScore();
+    isGameOver(pScore, cScore);
 }
 
 function printWhoWon(playerWon) {
@@ -59,5 +67,24 @@ function printWhoWon(playerWon) {
         result.textContent = "The game is a tie.";
     } else {
         result.textContent = "You lose!";
+    }
+}
+
+function updateScore(playerWon) {
+    playerWon === "player" ? pScore++ : playerWon === "computer" ? cScore++ : 0;
+}
+
+function printScore() {
+    playerScoreHolder.textContent = pScore;
+    computerScoreHolder.textContent = cScore;
+}
+
+function isGameOver(playerScore, computerScore) {
+
+    if(playerScore === 5 || computerScore === 5) {
+        let winner = playerScore === 5 ? "The player" : "The computer";
+        pScore = 0;
+        cScore = 0;
+        result.textContent = `The game is over. ${winner} has won the game!`;
     }
 }
